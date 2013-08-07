624f92cfe4071b78e474380fc2e3191564f6ecb5
phoenix6 <xiaofangsun1990@gmail.com>
2013-08-07 00:33:04 -0400

    Second commit

--- src/main/java/org/matin/client/WebServiceTest.java
+++ src/main/java/org/matin/client/WebServiceTest.java
@@ -27,24 +27,6 @@ public class WebServiceTest {
 		  Material returnedMaterial = restTemplate.postForObject( "http://localhost:8080/matIN/rest/material", request, Material.class );
 		  
 		  System.out.println(returnedMaterial.getName());
-	/* 
-		 Material material = new Material();
-		 material.setName("Al");
-		 material.setDescription("This is just an example");
-		 List<String> img = new ArrayList<String>();
-		 img.add("test.jpg");
-		 img.add("sample.jpg");
-		 List<String> ref = new ArrayList<String>();
-		 ref.add("www");
-		 ref.add("org");
-		 material.setImageURLs(img);
-		 material.setReferenceURLs(ref);
-		 
-		 Gson gson = new Gson();
-		 String s = gson.toJson(material);
-		 System.out.println(s);
-		 
-		 */
 		 
 	 }
 
